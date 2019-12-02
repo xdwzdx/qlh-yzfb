d496e2d0a7ce27944a5c06dfc3d557a2f386bb65
Tedu <Tedu@NOW8D8P9NF6O3ZU>
2019-12-02 15:34:51 +0800

    test commit

---
+++ a/src/a/a.java
@@ -0,0 +1,12 @@
+package a;
+
+public class a {
+	public static void main(String[] args) {
+		int i = 5;
+		int b = i++;
+		int c = ++i;
+		System.out.println(i);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
